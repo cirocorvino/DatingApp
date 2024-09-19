705c879b5140b866dcd9b9cb2b91cb64ec861d56
cirocorvino <ciro.corvino@gmail.com>
2024-09-19 18:50:18 +0200

    FE - photo handling (add, remove, setMain) + added n2-file-upload 3d party lib

--- client/DatingAppFE/src/app/_services/account.service.ts
+++ client/DatingAppFE/src/app/_services/account.service.ts
@@ -19,8 +19,7 @@ export class AccountService {
     return this.http.post<User>(this.urlAccountServce + 'login', model).pipe(
       map( user => {
         if(user){
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
         return user;
       })
@@ -31,14 +30,17 @@ export class AccountService {
     return this.http.post<User>(this.urlAccountServce + 'register', model).pipe(
       map( user => {
         if(user){
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
-        return user;
       })
     )
   }
-  
+
+  setCurrentUser(user: User) {
+    localStorage.setItem("user", JSON.stringify(user));
+    this.currentUser.set(user);
+  }
+
   logout(){
     localStorage.removeItem("user");
     this.currentUser.set(null);
