e76ff6936921f7312e96e8ee1f0329f0fe179a73
cirocorvino <ciro.corvino@gmail.com>
2024-09-21 18:00:56 +0200

    FE - implemented cache for paginated list of members and filters (used signals)

--- client/DatingAppFE/src/app/members/member-list/member-list.component.ts
+++ client/DatingAppFE/src/app/members/member-list/member-list.component.ts
@@ -2,8 +2,6 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { MemberCardComponent } from "../member-card/member-card.component";
 import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { AccountService } from '../../_services/account.service';
-import { UserParams } from '../../_models/userParams';
 import { FormsModule } from '@angular/forms';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
@@ -16,9 +14,7 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
 })
 export class MemberListComponent implements OnInit{
   
-  private accountService = inject(AccountService);
   membersService = inject(MembersService);
-  userParams = new UserParams(this.accountService.currentUser());
   genderList = [{value:'male', display:'Males'}, {value: 'female', display:'Females'}]
     
   
@@ -27,17 +23,17 @@ export class MemberListComponent implements OnInit{
   }
 
   loadMembers(){
-    this.membersService.getMembers(this.userParams);
+    this.membersService.getMembers();
   }
 
   resetFilters(){
-    this.userParams = new UserParams(this.accountService.currentUser());
+    this.membersService.resetUserParams();
     this.loadMembers();
   }
 
   pageChanged(event: any){
-    if(this.userParams.pageNumber !== event.page){
-      this.userParams.pageNumber = event.page;
+    if(this.membersService.userParams().pageNumber !== event.page){
+      this.membersService.userParams().pageNumber = event.page;
       this.loadMembers();
     }
   }
