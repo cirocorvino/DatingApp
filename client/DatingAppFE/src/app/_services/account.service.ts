e7387a140b7de5c5d04641f656dc15353cbbd640
cirocorvino <ciro.corvino@gmail.com>
2024-09-23 11:24:09 +0200

    FE - admin panel for user management + ngx-bootstrap modal libs

--- client/DatingAppFE/src/app/_services/account.service.ts
+++ client/DatingAppFE/src/app/_services/account.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { map } from 'rxjs';
 import { User } from '../_models/user';
 import { environment } from '../../environments/environment';
@@ -17,6 +17,14 @@ export class AccountService {
   urlAccountServce = this.urlBase + 'account/';
 
   currentUser = signal<User | null>(null);
+  roles = computed(() => {
+    const user = this.currentUser();
+    if(user && user.token){
+      const role = JSON.parse(atob(user?.token.split('.')[1])).role;
+      return Array.isArray(role) ? role : [role];
+    }
+    return [];
+  })
   
   login(model : any){
     return this.http.post<User>(this.urlAccountServce + 'login', model).pipe(
